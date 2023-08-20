7fdf116b97c650c58d12c4dded3361cf7093a26f
Cas de Bruijn <71756854+CasdeBruijnEHB@users.noreply.github.com>
2023-08-20 12:35:42 +0200

    feat: add backbutton

--- devportfolio/app/searchpage/page.js
+++ devportfolio/app/searchpage/page.js
@@ -6,6 +6,7 @@ import ButtonComp from "@/components/buttonComponent";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart as solidHeart } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as regularHeart } from '@fortawesome/free-regular-svg-icons';
+import { useRouter } from 'next/router'
 
 
 
